690bb293b4523c4327e47adf60724545ef71907d
MR0808 <kram@grebnesor.com>
2024-07-26 16:51:35 +1000

    finished backend

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,13 +1,17 @@
 const isAuth = (req, res, next, access) => {
+    if (!req.session.isLoggedIn && access === 'admin') {
+        return res.redirect('/admin/login');
+    }
+    if (!req.session.isLoggedIn) {
+        return res.redirect('/login');
+    }
     if (access === 'verified' && !req.session.user.isVerified) {
         return res.redirect('/?notVerified=true');
     }
     if (access === 'admin' && req.session.user.access !== 'Admin') {
         return res.redirect('/');
     }
-    if (!req.session.isLoggedIn) {
-        return res.redirect('/login');
-    }
+
     next();
 };
 
